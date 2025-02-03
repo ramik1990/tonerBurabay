30d43c887ab6271c3fd6a58bd0a25d1b2663c5d0
ramik1990 <ramikshudabaev@gmail.com>
2025-02-03 21:51:47 +0500

    Feat: Вынес календарь в отдельный компонент, стилизовал его

--- resources/js/Components/RecordPage.vue
+++ resources/js/Components/RecordPage.vue
@@ -1,70 +1,30 @@
 <template>
     <h1 align="center">Запись</h1>
-    <table border="1" v-if="calendarOnMonth.length">
-        <thead>
-            <tr>
-                <th>Понедельник</th>
-                <th>Вторник</th>
-                <th>Среда</th>
-                <th>Четверг</th>
-                <th>Пятница</th>
-                <th>Суббота</th>
-                <th>Воскресенье</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(week, index) in getWeeks()" :key="index">
-                <td v-for="day in week" :key="day?.day" :class="day?.is_weekend ? 'holiday' : ''">
-                    {{ day ? day.day : '' }}
-                </td>
-            </tr>
-        </tbody>
-    </table> 
-    <h3 v-else align="center">Загрузка...</h3>   
+    <calendar :year="year" :month="month"/>
 </template>
 
 <script>
-import axios from 'axios';
+import Calendar from './RecordPage/Calendar.vue';
 
 export default {
     name: "RecordPage",
     data() {
         return {
-            calendarOnMonth: []
+            year: null,
+            month: null
         }
+    },
+    components: {
+        'calendar' : Calendar
     },
     methods: {
-        getCalendar(year, month) {
-            axios.get(`/api/calendar/${year}/${month}`).then( response => {
-                this.calendarOnMonth = response.data;
-            });
-        },
-        getWeeks(){
-            if (!this.calendarOnMonth || this.calendarOnMonth.length === 0) return [];
-            let weeks = [];
-            let week = [];
-            let firstDayofMonth = this.calendarOnMonth[0];
-            let firstDayIndex = this.getDayIndex(firstDayofMonth.day_of_week);
-            for(let i = 0; i < firstDayIndex; i++) {
-                week.push(null);
-            }
-            this.calendarOnMonth.forEach((day) => {
-                week.push(day);
-                if(day.day_of_week === 'Sunday') {
-                    weeks.push(week);
-                    week = [];
-                }
-            });
-            if (week.length) weeks.push(week);
-            return weeks;
-        },
-        getDayIndex(day) {
-            const daysofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
-            return daysofWeek.indexOf(day);            
+        getCurrentDate() {
+            this.year = new Date().getFullYear();
+            this.month = new Date().getMonth() + 1;
         }
     },
-    mounted() {
-        this.getCalendar(new Date().getFullYear(), new Date().getMonth() + 1);
+    created() {
+        this.getCurrentDate();
     }
 }
 </script>
@@ -74,17 +34,5 @@ h1 {
     text-transform: uppercase;
     font-weight: bolder;
     font-family: monospace;
-}
-table {
-    margin: auto;
-    width: 80%;
-}
-table td {
-    width: 12.7%;
-    height: 100px;
-    text-align: center;
-}
-.holiday {
-    background-color: rgb(255, 219, 219);
 }
 </style>
---
+++ resources/js/Components/RecordPage/Calendar.vue
@@ -0,0 +1,106 @@
+<template>
+    <table border="1" v-if="calendarOnMonth.length">
+        <thead>
+            <tr>
+                <th>ПН</th>
+                <th>ВТ</th>
+                <th>СР</th>
+                <th>ЧТ</th>
+                <th>ПТ</th>
+                <th>СБ</th>
+                <th>ВС</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(week, index) in getWeeks()" :key="index">
+                <td v-for="day in week" :key="day?.day" :class="day?.is_weekend ? 'holiday' : ''">
+                    {{ day ? day.day : '' }}
+                </td>
+            </tr>
+        </tbody>
+    </table> 
+    <h3 v-else align="center">Загрузка...</h3> 
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Calendar",
+    props: {
+        year: Number,
+        month: Number
+    },
+    data() {
+        return {
+            calendarOnMonth: []
+        }
+    },
+    methods: {
+        getCalendar(year, month) {
+            axios.get(`/api/calendar/${year}/${month}`).then( response => {
+                this.calendarOnMonth = response.data;
+            });
+        },
+        getWeeks(){
+            if (!this.calendarOnMonth || this.calendarOnMonth.length === 0) return [];
+            let weeks = [];
+            let week = [];
+            let firstDayofMonth = this.calendarOnMonth[0];
+            let firstDayIndex = this.getDayIndex(firstDayofMonth.day_of_week);
+            for(let i = 0; i < firstDayIndex; i++) {
+                week.push(null);
+            }
+            this.calendarOnMonth.forEach((day) => {
+                week.push(day);
+                if(day.day_of_week === 'Sunday') {
+                    weeks.push(week);
+                    week = [];
+                }
+            });
+            if (week.length) weeks.push(week);
+            return weeks;
+        },
+        getDayIndex(day) {
+            const daysofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday'];
+            return daysofWeek.indexOf(day);            
+        }
+    },
+    mounted() {
+        this.getCalendar(this.year, this.month);
+    }
+}
+</script>
+
+<style scoped>
+table {
+    margin: auto;
+    min-width: 350px;
+    width: 80%;
+    border: none;
+}
+table thead tr th{
+    border: none;
+    background-color: #4D4D4D;
+    text-transform: uppercase;
+    color: white;
+    font-family: 'Courier New', Courier, monospace;
+    font-weight: bolder;
+    height: 30px;
+}
+table td {
+    width: 12.7%;
+    height: 50px;
+    text-align: center;
+    border: none;
+    background-color: rgb(245, 245, 245);
+}
+table td:not(:empty):hover {
+    transform: scale(1.05);
+    box-shadow: #4D4D4D 0 0 2px;
+    cursor: pointer;
+}
+.holiday {
+    background-color: rgb(255, 219, 219);
+}
+</style>
